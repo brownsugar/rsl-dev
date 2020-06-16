118e21b6a50f1149b2e99ac3d8be3d9e161a4ff4
Lay <brownsugar.idv.tw@gmail.com>
2020-06-16 16:43:55 +0800

    move host data to single file

---
+++ web/client/assets/data/season1/hosts.js
@@ -0,0 +1,97 @@
+const hosts = [
+  {
+    name: '蛋蛋',
+    alias: 'DanDan',
+    title: '知名聯賽主播',
+    avatar: 'dandan.jpg',
+    link: 'https://www.youtube.com/user/cvb3027',
+    experience: `-播報經驗-
+2017 世界賽資格賽評
+2018 STO各季主播/賽評
+2018 STO春、夏總決賽主播/賽評
+2018 STO冬季資格賽主播/賽評
+2018 STO總決賽賽評
+2018 SCL春季總決賽賽評
+2018 新北電競館開幕賽評
+2018 Shadowverse世界大賽主播/賽評
+2019 STO秋季賽季賽主播
+2019 跑跑卡丁車曜越聯賽賽評
+2019 Shadowverse世界大賽主播
+
+-賽事經驗-
+跑跑卡丁車：
+2012 北區甲組聯賽亞軍
+2012 樂豆公開賽北區八強
+2012 樂豆全國總決賽八強
+
+英雄聯盟：
+2014 校際盃大專組全國八強
+2015 校際盃大專組全國八強
+2016 校際盃大專組全國八強
+
+闇影詩章：
+2017 4Gamers大師爭霸賽 冠軍
+2017 詩涼子邀請賽 周冠軍
+2018 新加坡公開賽 參賽選手
+2018 SCL國際邀請賽 參賽選手
+2019 日本RAGE秋季賽 台灣第一位打進Day2選手
+
+寶可夢 PTCG：
+2019 台灣1500人地區大賽 TOP 32`
+  },
+  {
+    name: '爆哥',
+    alias: 'NeaL',
+    title: '競速專業講評',
+    avatar: 'neal.jpg',
+    link: 'https://www.youtube.com/channel/UCi7eKRJZvxMhwhiWyARZtjg',
+    experience: `-播報經驗-
+跑跑卡丁車：
+2019 TT曜越電競聯賽 總冠軍賽 賽評
+
+極速領域：
+2019 春季錦標賽 賽評
+2019 亞洲盃總冠軍賽 主播
+2019 兆豐銀行盃 主播
+2020 冬季明星表演賽 賽評
+
+-賽事經驗-
+2019 跑跑卡丁車世界爭霸賽 個人賽冠軍
+2019 Global SuperMatch 亞軍`
+  },
+  {
+    name: 'Space Q',
+    alias: '',
+    title: '校園五速嘴解說',
+    avatar: 'spaceq.jpg',
+    link: '',
+    experience: `-播報經歷-
+2016 逢甲耶誕前夕電競嘉年華會冠軍賽主播
+2016 UGT 校園電競賽事英雄聯盟賽評
+2016 GIGI 電競聯盟公益慈善盃英雄聯盟賽評
+2016 優九聯盟第一屆電競聯賽 英雄聯盟項目賽評
+2016 快樂霸主英雄聯盟邀請賽賽評
+2017 Garena校際盃 英雄聯盟項目主播兼賽評（崑山、海洋、北科）
+2018 Garena校際盃 英雄聯盟項目主播兼賽評（崑山、聯合、勤益）
+2018 第一屆、第二屆 ESR 天使聯賽賽評
+2018 第三屆、第四屆 北區電競聯賽賽評
+2018 ROG MASTER 英雄聯盟邀請賽賽評
+2018 CGA 女子賽賽評
+2018 台北國際電玩展 曜越跑跑卡丁車賽事主播
+2019 TT曜越電競聯賽 車神競速系列 跑跑卡丁車賽事主播`
+  },
+  {
+    name: '保羅',
+    alias: 'Jensen',
+    title: '競速冠軍選手',
+    avatar: 'jensen.jpg',
+    link: 'https://www.youtube.com/channel/UCysszMfLmSgoWaz_kfOOp-g',
+    experience: `-賽事經驗-
+2014 K1 電視聯賽 團體賽冠軍
+2018 AKEL 曜越網咖盃電競聯賽 團體賽冠軍
+2018 TKL 夢想盃跑跑聯賽個人賽 亞軍
+2019 TT曜越電競聯賽 個人賽 季軍`
+  }
+]
+
+export default hosts
