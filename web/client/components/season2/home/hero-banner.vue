bca97a342c58d9ce291daff577cd4ee225a407a8
Lay <brownsugar.idv.tw@gmail.com>
2021-06-14 03:53:24 +0800

    add banner parallax effect & loop animation

--- web/client/components/season2/home/hero-banner.vue
+++ web/client/components/season2/home/hero-banner.vue
@@ -1,9 +1,10 @@
 <template>
   <div
+    ref="scene"
     class="hero-banner"
     :class="{ ready }"
   >
-    <div class="stripes">
+    <div class="stripes" data-depth="0.1">
       <div class="horizontal">
         <div class="stripe left" />
         <div class="stripe right" />
@@ -32,24 +33,28 @@
       </div>
     </div>
     <div class="main">
-      <div class="rsl">
+      <div class="rsl" data-depth="0.4">
         <img src="~/assets/images/rsl/rsl-color-full.svg" alt="RSL 賽事聯盟">
         <span>RSL 賽事聯盟</span>
       </div>
-      <h1 class="title">
-        <img src="~/assets/images/season2/home/hero_title.svg" alt="夢想盃">
-        <span>夢想盃跑跑聯賽 Season 2</span>
-      </h1>
-      <div class="subtitle">
-        <img src="~/assets/images/season2/home/hero_subtitle.svg" alt="跑跑聯賽 Season 2">
+      <div class="titles" data-depth="0.6">
+        <h1 class="title">
+          <img src="~/assets/images/season2/home/hero_title.svg" alt="夢想盃">
+          <span>夢想盃跑跑聯賽 Season 2</span>
+        </h1>
+        <div class="subtitle mx-auto">
+          <img src="~/assets/images/season2/home/hero_subtitle.svg" alt="跑跑聯賽 Season 2">
+        </div>
       </div>
-      <h2 class="slogan">
-        <img class="left" src="~/assets/images/season2/home/hero_slogan_left.svg" alt="夢想啟程">
-        <img class="middle" src="~/assets/images/season2/home/hero_slogan_x.svg" alt="，">
-        <img class="right" src="~/assets/images/season2/home/hero_slogan_right.svg" alt="再造傳奇">
+      <h2 class="slogan" data-depth="0.5">
+        <div class="text-wrap">
+          <img class="left" src="~/assets/images/season2/home/hero_slogan_left.svg" alt="夢想啟程">
+          <img class="middle" src="~/assets/images/season2/home/hero_slogan_x.svg" alt="，">
+          <img class="right" src="~/assets/images/season2/home/hero_slogan_right.svg" alt="再造傳奇">
+        </div>
         <span>夢想啟程，再造傳奇</span>
       </h2>
-      <div class="restart">
+      <div class="restart" data-depth="0.4">
         RE:START KARTRIDER LEAGUE
       </div>
     </div>
@@ -63,10 +68,10 @@
           md="2"
           offset-md="2"
         >
-          <div class="character dao">
+          <div class="character dao" data-depth="0.2">
             <img src="~/assets/images/season2/home/hero_dao.svg">
           </div>
-          <div class="character uni">
+          <div class="character uni" data-depth="0.3">
             <img src="~/assets/images/season2/home/hero_uni.svg">
           </div>
         </v-col>
@@ -75,10 +80,10 @@
           md="2"
           offset-md="4"
         >
-          <div class="character dizni">
+          <div class="character dizni" data-depth="0.2">
             <img src="~/assets/images/season2/home/hero_dizni.svg">
           </div>
-          <div class="character bazzi">
+          <div class="character bazzi" data-depth="0.3">
             <img src="~/assets/images/season2/home/hero_bazzi.svg">
           </div>
         </v-col>
@@ -88,20 +93,32 @@
 </template>
 
 <script>
+import Parallax from 'parallax-js'
+import { randomInt } from '~/assets/utils/random'
+
 export default {
   name: 'HeroBanner',
   components: {},
   props: {},
   data: () => ({
-    ready: false
+    ready: false,
+    parallax: null
   }),
   computed: {},
   watch: {},
   mounted () {
-    this.startAnimate()
+    this.initBanner()
+  },
+  beforeDestroy () {
+    this.parallax && this.parallax.destroy()
   },
   methods: {
-    startAnimate () {
+    async initBanner () {
+      await this.startIntro()
+      this.startAnimationLoop()
+      this.initParallax()
+    },
+    startIntro () {
       const durationCommon = 1000
       const durationHalf = durationCommon / 2
 
@@ -159,7 +176,12 @@ export default {
         this.$anime.timeline({
           easing: 'easeOutElastic',
           duration: durationCommon,
-          autoplay: false
+          autoplay: false,
+          update (anim) {
+            if (anim.progress > 70 && sceneMain.paused) {
+              sceneMain.play()
+            }
+          }
         })
           .add({
             targets: '.hero-banner .ribbons .thin img',
@@ -173,7 +195,6 @@ export default {
             targets: '.hero-banner .ribbons .bottom .thick img',
             translateY: ['100%', '0%']
           }, durationHalf)
-      sceneRibbons.finished.then(() => sceneMain.play())
 
       const sceneMain =
         this.$anime.timeline({
@@ -263,6 +284,62 @@ export default {
 
       sceneStripes.play()
       this.ready = true
+
+      return sceneCharacter.finished
+    },
+    startAnimationLoop () {
+      // Animation for titles
+      const makeTitleAniOptions = (targets) => {
+        return {
+          targets,
+          scale: [
+            1,
+            1.1,
+            0.98,
+            1
+          ]
+        }
+      }
+      this.$anime.timeline({
+        easing: 'easeInBack',
+        duration: 1200,
+        delay: 7000,
+        loop: true
+      })
+        .add(makeTitleAniOptions('.hero-banner .title img'))
+        .add(makeTitleAniOptions('.hero-banner .subtitle img'), 200)
+        .add(makeTitleAniOptions('.hero-banner .slogan .text-wrap'), 400)
+
+      // Animation for characters
+      const makeCharAniOptions = (targets) => {
+        const rotateDeg = randomInt(4, 8)
+        const delay = randomInt(3000, 8000)
+
+        return {
+          targets,
+          easing: 'easeInOutSine',
+          duration: 600,
+          delay,
+          rotate: [
+            rotateDeg * -1,
+            rotateDeg,
+            rotateDeg / -2,
+            rotateDeg / 2,
+            0
+          ],
+          loop: true
+        }
+      }
+      this.$anime(makeCharAniOptions('.hero-banner .characters .dao img'))
+      this.$anime(makeCharAniOptions('.hero-banner .characters .dizni img'))
+      this.$anime(makeCharAniOptions('.hero-banner .characters .uni img'))
+      this.$anime(makeCharAniOptions('.hero-banner .characters .bazzi img'))
+    },
+    initParallax () {
+      const scene = this.$refs.scene
+      this.parallax = new Parallax(scene, {
+        selector: '[data-depth]'
+      })
     }
   }
 }
@@ -290,13 +367,19 @@ export default {
     visibility: visible;
   }
   img {
+    backface-visibility: hidden;
     pointer-events: none;
+  }
+  [data-depth] {
+    position: relative !important; // Override Parallax.js attribute style
   }
 }
 .stripes {
   $h-stripe-y-offset: 25px;
   $all-stripe-y-offset: 50px;
   $flex-basis: 47.5;
+
+  mix-blend-mode: overlay;
 
   .horizontal,
   .vertical {
@@ -310,9 +393,6 @@ export default {
   }
   .vertical {
     flex-direction: column;
-  }
-  .stripe {
-    mix-blend-mode: overlay;
   }
   .top,
   .bottom {
@@ -377,6 +457,7 @@ export default {
   }
 }
 .main {
+  z-index: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -399,11 +480,16 @@ export default {
     transform: translateY(-40px);
   }
   .slogan {
-    display: flex;
-    justify-content: space-between;
     width: 450px;
     height: 35px;
     margin-top: 10px;
+
+    .text-wrap {
+      display: flex;
+      justify-content: space-between;
+      height: 100%;
+      backface-visibility: hidden;
+    }
   }
   .restart {
     margin-top: 110px;
@@ -425,18 +511,18 @@ export default {
     margin-bottom: 20px;
   }
   .dao {
-    width: 295px;
+    width: 280px;
   }
   .uni {
-    width: 340px;
-    transform: translateX(-60px);
+    left: -60px !important; // Override Parallax.js attribute style
+    width: 320px;
   }
   .dizni {
-    width: 300px;
+    width: 280px;
   }
   .bazzi {
-    width: 350px;
-    transform: translateX(60px);
+    left: 60px !important; // Override Parallax.js attribute style
+    width: 330px;
   }
 }
 </style>
