4373db83ff510da4e3284abea4c6c5461a12e240
Lay <brownsugar.idv.tw@gmail.com>
2021-06-13 17:40:58 +0800

    fix "res.set" is not a native function in Node.js

--- web/server/endpoint/proxy.js
+++ web/server/endpoint/proxy.js
@@ -4,5 +4,8 @@ import config from 'config'
 export default async function (req, res, next) {
   const url = config.api.url + req.originalUrl
   const { headers, data } = await axios.get(url)
-  return res.set(headers).end(data)
+  Object.keys(headers).forEach((key) => {
+    res.setHeader(key, headers[key])
+  })
+  return res.end(data)
 }
