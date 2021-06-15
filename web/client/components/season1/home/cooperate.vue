a3849ba7646c62128b576542703e78b92079f2c5
Lay <brownsugar.idv.tw@gmail.com>
2021-06-15 18:13:11 +0800

    move cooperators of Season 1 to single file

--- web/client/components/season1/home/cooperate.vue
+++ web/client/components/season1/home/cooperate.vue
@@ -68,6 +68,7 @@
 <script>
 import PrimaryTitle from '~/components/common/primary-title'
 import ActionAlert from '~/components/common/action-alert'
+import cooperators from '~/data/season1/cooperators'
 
 export default {
   name: 'Cooperate',
@@ -76,116 +77,7 @@ export default {
     ActionAlert
   },
   data: () => ({
-    cooperators: [
-      {
-        title: '指導單位',
-        data: [
-          {
-            name: '南台科技大學資訊傳播系',
-            logo: {
-              file: 'stust.png',
-              height: 60
-            },
-            link: 'https://www.facebook.com/231508650386604'
-          },
-          {
-            name: '靜宜大學資訊傳播工程學系',
-            logo: {
-              file: 'pu.png',
-              height: 60
-            },
-            link: 'https://www.facebook.com/pucsceofficalpage'
-          },
-          {
-            name: '競及電競',
-            logo: {
-              file: 'ulevelup.png',
-              height: 80
-            },
-            link: 'https://ulevelup.com'
-          },
-          {
-            name: '中華電子競技教育學會',
-            logo: {
-              file: 'ceeatw.png',
-              height: 80
-            },
-            link: 'http://www.ceeatw.org'
-          }
-        ]
-      },
-      {
-        title: '協辦單位',
-        data: [
-          {
-            name: '遊戲橘子',
-            logo: {
-              file: 'gamania.png',
-              height: 60
-            },
-            link: 'https://www.facebook.com/kartriderTW'
-          }
-        ]
-      },
-      {
-        title: '執行單位',
-        data: [
-          {
-            name: 'RSL 賽事聯盟',
-            logo: {
-              file: 'rsl.png',
-              height: 100
-            },
-            link: 'https://rsl.tw'
-          }
-        ]
-      },
-      {
-        title: '合作夥伴',
-        data: [
-          {
-            name: '掠奪者 Predator',
-            logo: {
-              file: 'predator.png',
-              height: 80
-            },
-            link: 'https://www.facebook.com/PredatorGamingTW'
-          },
-          {
-            name: 'Acer',
-            logo: {
-              file: 'acer.png',
-              height: 35
-            },
-            link: 'https://www.acer.com'
-          },
-          {
-            name: 'Cooler Master',
-            logo: {
-              file: 'coolermaster.png',
-              height: 60
-            },
-            link: 'https://apac.coolermaster.com/tw/peripheral/mice/mm710/'
-          },
-          {
-            name: 'HyperX',
-            logo: {
-              file: 'hyperx.png',
-              height: 55
-            },
-            link: 'https://hyperx.gg/kerdinter'
-          },
-          {
-            name: '賽德斯 Sades',
-            logo: {
-              file: 'sades.png',
-              height: 80
-            },
-            link: 'https://www.sades.com.tw'
-          }
-        ]
-      }
-    ]
+    cooperators
   })
 }
 </script>
