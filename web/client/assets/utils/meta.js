dc0c5e343439595265437eed47b1f1d09df856e3
Lay <brownsugar.idv.tw@gmail.com>
2021-08-05 10:38:10 +0800

    fix meta tag description attribute name

--- web/client/assets/utils/meta.js
+++ web/client/assets/utils/meta.js
@@ -16,14 +16,14 @@ const genarateMetaInfo = ({ vm, staticPages, themeColor, ogImage, defaultTitle }
     meta: [
       { name: 'theme-color', content: themeColor },
       { name: 'apple-mobile-web-app-status-bar-style', content: themeColor },
-      { property: 'publisher', content: vm.$config.rsl.facebook.url },
       { name: 'author', content: vm.$config.rsl.name },
+      { name: 'description', content: vm.site.description, hid: 'description' },
+      { property: 'publisher', content: vm.$config.rsl.facebook.url },
       { property: 'og:site_name', content: title },
       { property: 'og:title', template: titleWithChunk, hid: 'og:title' },
       { property: 'og:type', content: ogType, hid: 'og:type' },
       { property: 'og:url', content: url, hid: 'og:url' },
       { property: 'og:description', content: vm.site.description, hid: 'og:description' },
-      { property: 'description', content: vm.site.description, hid: 'description' },
       { property: 'og:image', content: ogImage, hid: 'og:image' }
     ]
   }
