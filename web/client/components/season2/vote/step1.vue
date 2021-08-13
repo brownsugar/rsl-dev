7f9ea2f487516c467b53dc85679a127507a97f9b
Lay <brownsugar.idv.tw@gmail.com>
2021-08-14 02:47:34 +0800

    update voting event description

--- web/client/components/season2/vote/step1.vue
+++ web/client/components/season2/vote/step1.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="vote">
-    <h4 class="text-h6 mb-2">
+    <h4 class="text-h6 font-weight-bold mb-2">
       活動說明
     </h4>
     <p>RSL 夢想盃跑跑聯賽正式進入 8 強賽環節，所有 8 強賽隊伍所屬選手都將成為人氣王候選人，而所有觀眾都能透過投票來支持自己喜愛的選手，幫助他拿下本季聯賽人氣王獎項！</p>
     <p>為了保障票選活動公平性，本季人氣王票選活動採用實名制計票，所有投票參與人需先完成手機號碼驗證後方可進行投票，每個手機號碼可以投下一票，僅支援台灣手機號碼投票，此驗證流程費用由 RSL 賽事聯盟承擔，參與人不需額外支付費用。</p>
-    <h4 class="text-h6 mb-2">
+    <p>最終人氣王票選結果將於 8/29 總決賽轉播時公佈，敬請準時收看！</p>
+    <h4 class="text-h6 font-weight-bold mb-2">
       活動時間
     </h4>
     <p>{{ eventDuration }}</p>
-    <h4 class="text-h6 mb-2">
+    <h4 class="text-h6 font-weight-bold mb-2">
       驗證規則
     </h4>
     <ul class="mb-4">
       <li>每個手機號碼擁有一次投票資格。</li>
       <li>每個手機號碼的簡訊驗證碼請求次數上限為 <b>{{ getVoteField('max_request') }} 次</b>，達到上限時無法再請求簡訊驗證碼。</li>
-      <li>每個手機號碼的簡訊驗證碼回填次數上限為 <b>{{ getVoteField('max_attempt') }} 次</b>，達到上限時喪失投票資格。</li>
+      <li>每個手機號碼的簡訊驗證碼回填錯誤次數上限為 <b>{{ getVoteField('max_attempt') }} 次</b>，達到上限時喪失投票資格。</li>
     </ul>
-    <h4 class="text-h6 mb-2">
+    <h4 class="text-h6 font-weight-bold mb-2">
       個人資料保護聲明
     </h4>
     <p>您所提供的手機號碼僅限於本投票活動驗證使用，所有資料將於活動結束後統一消除。</p>
