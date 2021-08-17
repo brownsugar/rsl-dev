e8e20996469335e62ad9e828f987bef0a8aa6771
Lay <brownsugar.idv.tw@gmail.com>
2021-08-17 17:06:33 +0800

    prevent conflict branding color with old one

--- web/client/assets/theme/season1.js
+++ web/client/assets/theme/season1.js
@@ -4,7 +4,11 @@ export default {
   themes: {
     light: {
       primary: '#D86A60',
-      secondary: colors.lightBlue.accent4
+      secondary: colors.lightBlue.accent4,
+      facebook: '#1877F2',
+      youtube: '#FF0200',
+      twitch: '#9147FF',
+      discord: '#7289DA'
     },
     dark: {
     }
--- web/client/assets/theme/season2.js
+++ web/client/assets/theme/season2.js
@@ -6,7 +6,11 @@ export default {
       primary: '#FF3C00',
       secondary: '#FF8700',
       tertiary: '#FFCD00',
-      'grey-dark': '#333'
+      'grey-dark': '#333',
+      facebook: '#1877F2',
+      youtube: '#FF0000',
+      twitch: '#9146FF',
+      discord: '#5865F2'
     },
     dark: {
     }
